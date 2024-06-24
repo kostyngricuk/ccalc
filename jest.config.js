63a637289aa9f5130d27cac81690536e02d6cf46
kanstantsin.hrytsuk <kanstantsin.hrytsuk@gmail.com>
2024-06-24 16:50:41 +0300

    feature: first test

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  testEnvironment: 'jsdom',
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    "^@components/(.*)$": "<rootDir>/src/components/$1",
+    "^@icons/(.*)$": "<rootDir>/src/icons/$1",
+    "^@pages/(.*)$": "<rootDir>/src/pages/$1",
+    "^@services/(.*)$": "<rootDir>/src/services/$1",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
+  },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  },
+  setupFilesAfterEnv: [
+    './setupTests.ts'
+  ],
+};
