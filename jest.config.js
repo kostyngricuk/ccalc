a4f37cd89c8c27761fcc2714dc507c0f92c9e870
kanstantsin.hrytsuk <kanstantsin.hrytsuk@gmail.com>
2024-07-17 10:41:52 +0300

    feature: set test limit

--- jest.config.js
+++ jest.config.js
@@ -19,4 +19,12 @@ module.exports = {
     './setupTests.ts'
   ],
   moduleDirectories: ["node_modules", "src"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
